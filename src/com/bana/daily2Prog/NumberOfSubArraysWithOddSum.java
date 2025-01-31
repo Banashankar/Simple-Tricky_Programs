b2566762fe105991efc78edda3bd706d29ef7630
Banashankar <banashankar70@gmail.com>
2025-01-31 23:25:52 +0530

    added meaningful sys outs for understanding of number of sub arrays with odd sum

--- src/com/bana/daily2Prog/NumberOfSubArraysWithOddSum.java
+++ src/com/bana/daily2Prog/NumberOfSubArraysWithOddSum.java
@@ -14,20 +14,35 @@ public class NumberOfSubArraysWithOddSum {
 		// TODO Auto-generated method stub
 		int arr[] = {1,3,5};
 
-		System.out.println(countOddSumSubArr(arr));
+		int result = countOddSumSubArr(arr);
+		System.out.println("Count of subbArrays with odd sums: " + result);
 	}
 
 	private static int countOddSumSubArr(int[] arr) {
+		List<List<Integer>> subArrays = new ArrayList<>();
+		List<Integer> subArraySums = new ArrayList<>();
+		List<Integer> oddSums = new ArrayList<>();
 		int count = 0;
 		for(int i=0;i<arr.length;i++) {
 			int sum = 0;
 			for(int j=i;j<arr.length;j++) {
 				sum = sum + arr[j];
+				//Collecting subArrays
+				List<Integer> subarray = new ArrayList<>();
+				for(int k=i;k<=j;k++) {
+					subarray.add(arr[k]);
+				}
+				subArrays.add(subarray);
+				subArraySums.add(sum);
 				if(sum % 2 !=0) {
+					oddSums.add(sum);
 					count++;
 				}
 			}
 		}
+		System.out.println("All subArrays: " + subArrays);
+		System.out.println("All subArray Sums: " + subArraySums);
+		System.out.println("Odd sums: " + oddSums);
 		return count;
 	}
 
